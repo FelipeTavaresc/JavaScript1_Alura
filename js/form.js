bf7d6c3443d223e0f8425ae667e28a72bd9fc060
Felipe Tavares <felipe.tavares@outlook.com>
2018-03-03 22:28:42 -0300

    Aplicando Orientação a Objetos JS

--- js/form.js
+++ js/form.js
@@ -3,14 +3,14 @@ botaoAdicionar.addEventListener("click", function(event){
 	event.preventDefault();
 	
 	var form = document.querySelector("#form-adiciona");
+    //Função Obtem dados do formulário
+    var paciente = obtemPacienteDoFormulario(form);
 
-	var nome = form.nome.value;
-	var peso = form.peso.value;
-	var altura = form.altura.value;
-	var gordura = form.gordura.value;
+    console.log(paciente);
+    
+    
 
 	var pacienteTr = document.createElement("tr");
-
 	var nomeTd = document.createElement("td");
 	var pesoTd = document.createElement("td");
 	var alturaTd = document.createElement("td");
@@ -34,3 +34,17 @@ botaoAdicionar.addEventListener("click", function(event){
 	tabela.appendChild(pacienteTr);
 
 });
+
+function obtemPacienteDoFormulario(form){
+
+    var paciente = {
+        nome: form.nome.value,
+        peso: form.peso.value,
+        altura: form.altura.value,
+        gordura: form.gordura.value,
+        imc: calculaImc(form.peso.value, form.altura.value)
+    }
+
+    return paciente;
+
+}
